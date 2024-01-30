fd617e0e7ff501d32f0117727812058744edce8c
YutaNizuka <niduka81@gmail.com>
2024-01-31 00:06:49 +0900

    ファイルを読み込もう

---
+++ lesson01/sample16.php
@@ -0,0 +1,10 @@
+<?php
+$news = file_get_contents('data/news.txt');
+echo $news;
+
+//ファイルの追記
+$news = $news . "<br>追加のニュースです";
+$success = file_put_contents('data/news.txt',$news);
+
+// readfile('data/news.txt');
+?>
